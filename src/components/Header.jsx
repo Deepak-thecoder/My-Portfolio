1c3dcdc725ee86fcfdc75a1185d513f92e52d7e5
Deepak kumar sahu <kumarsahudeepak365@gmail.com>
2025-10-21 17:17:35 +0530

    contact me button is now functional

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import myPhoto from "../assets/Deepak.jpg"; 
 
 const Header = () => {
+
+  const scrollToContact = () => {
+    const contactSection = document.getElementById("contact");
+    if (contactSection) {
+      contactSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <header>
       <div className="header-container">
         <img src={myPhoto} alt="Deepak Kumar Sahu" className="profile-photo" />
         <h1>Deepak Kumar Sahu</h1>
         <p>Full Stack Developer | Java | Spring Boot | React</p>
-        <button>Contact Me</button>
+        <button onClick={scrollToContact}>Contact Me</button>
       </div>
     </header>
   );
